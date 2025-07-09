522c62dd7eb54c18fd8294803536ec9e270a7aa0
Lekhika <rlekhika.regmi@gmail.com>
2025-07-09 11:28:17 +0545

    Setting routes

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  //static is used because its same for all cases no matter the object's different features
+  //just to get single property of class without having to create an object
+  // can also be used for methods that is used from class without having to make object
+
+  static const String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(child: Image.asset('images/logo.png'), height: 60.0),
+                Text(
+                  'Flash Chat',
+                  style: TextStyle(fontSize: 45.0, fontWeight: FontWeight.w900),
+                ),
+              ],
+            ),
+            SizedBox(height: 48.0),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    //Go to login screen.
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text('Log In'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    //Go to registration screen.
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text('Register'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
